b3aa46b3271b114e28c3395a4fedb3088f98680c
floyd-vaz <floydvaz01042003@gmail.com>
2026-05-18 16:53:27 +0530

    feat: initial project setup with backend structure

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running on port 5000');
+    });
+  })
+  .catch((err) => console.log(err));
