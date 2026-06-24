3746960be6fa6f9cfc573b8c2d322d2dd77f7ae7
floyd-vaz <floydvaz01042003@gmail.com>
2026-06-24 16:45:58 +0530

    fix: update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://task-manager-app-hy56.onrender.com";
 export default API_URL;
